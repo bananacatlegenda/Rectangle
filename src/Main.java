c6fc597baca894bdd5691443fe588fcea3800d5c
bananacatlegenda <amalina101210@gmail.com>
2024-03-03 11:41:11 +0300

    update

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int num = new Scanner(System.in).nextInt();
+       Scanner  num1 = new Scanner(System.in);
+    }
+}
